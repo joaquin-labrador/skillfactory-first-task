1afeab1e0774063422bcba3cd9f7487acd471a5a
joaquin-labrador <joaquinlabrador1010@gmail.com>
2022-08-12 02:45:26 -0300

    added 🆕 : repository file for better arquitecture

--- src/repositories/products.repository.js
+++ src/repositories/products.repository.js
@@ -1,26 +1,26 @@
 import modelProducts from "../model/model.products";
 
 const getAllProducts = async () => {
-    return await modelProducts.getAllProducts();
-    }
+  return await modelProducts.getAllProducts();
+};
 
 const getProductById = async (id) => {
-    return await modelProducts.getProductById(id);
-}
+  return await modelProducts.getProductById(id);
+};
 const getCategories = async () => {
-    return await modelProducts.getCategories();
-}
+  return await modelProducts.getCategories();
+};
 const getProductByCategory = async (category) => {
-    return await modelProducts.getProductByCategory(category);
-}
-const productsInCategory = async () => {
-    return await modelProducts.productsInCategory();
-}
+  return await modelProducts.getProductByCategory(category);
+};
+const getProductsInCategory = async () => {
+  return await modelProducts.productsInCategory();
+};
 
 export default {
-    getAllProducts,
-    getProductById,
-    getCategories,
-    getProductByCategory,
-    productsInCategory,
-}
+  getAllProducts,
+  getProductById,
+  getCategories,
+  getProductByCategory,
+  getProductsInCategory,
+};
