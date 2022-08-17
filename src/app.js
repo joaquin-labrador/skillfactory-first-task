755ed66abee16dfe056f52e0b676818bcd754e3a
joaquin-labrador <joaquinlabrador1010@gmail.com>
2022-08-17 17:16:02 -0300

    C:/Program Files/Git/api/v1.0/ is ready 🎊

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import routerProducts from "./router/productsrouter.js";
 import routerUsers from "./router/usersrouter.js";
 import routerCarts from "./router/cartsrouter.js";
-import error404 from "./middleware/404.js";
+import errorHandler from "./middleware/errorhandler.js";
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -10,10 +10,10 @@ const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/api/", routerProducts, routerUsers, routerCarts);
+app.use("/api/v1.0", routerProducts, routerUsers, routerCarts);
 
 //error handler 404
-app.use(error404);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
